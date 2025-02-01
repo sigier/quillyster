94d5adc39c40f7e5545058bce48d9036ea99f8f9
sigier <jotulw@proton.me>
2025-02-01 18:13:48 +0200

    style: sidebar added

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -1,7 +1,18 @@
 export const AppLayout = ({ children }) => {
   return (
-    <div>
-      this is layout <div>{children}</div>
+    <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
+      <div className="flex flex-col overflow-hidden text-white">
+        <div className="bg-slate-800">
+          <div>lofo</div>
+          <div>cta</div>
+          <div>tokens</div>
+        </div>
+        <div className="flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-600">
+          List post
+        </div>
+        <div className="bg-cyan-600">user info</div>
+      </div>
+      <div>{children}</div>
     </div>
   );
 };
