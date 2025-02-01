acd97761597ed6eed3116a67a890efd0b892c330
sigier <jotulw@proton.me>
2025-02-01 18:27:30 +0200

    style: footer section added

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -1,4 +1,9 @@
+import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import Image from "next/image";
+
 export const AppLayout = ({ children }) => {
+  const { user } = useUser();
   return (
     <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
       <div className="flex flex-col overflow-hidden text-white">
@@ -10,7 +15,29 @@ export const AppLayout = ({ children }) => {
         <div className="flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-600">
           List post
         </div>
-        <div className="bg-cyan-600">user info</div>
+        <div className="bg-cyan-600 flex items-center gap-2 border-t border-t-black/50 h-20 px-2">
+          {!!user ? (
+            <>
+              <div className="min-w-[50px]">
+                <Image
+                  src={user.picture}
+                  alt={user.name}
+                  height={50}
+                  width={50}
+                  className="rounded-full"
+                />
+              </div>
+              <div className="flex-1">
+                <div className="font-bold">{user.email}</div>
+                <Link className="text-sm" href="/api/auth/logout">
+                  Logout
+                </Link>
+              </div>
+            </>
+          ) : (
+            <Link href="/api/auth/login">Login</Link>
+          )}
+        </div>
       </div>
       <div>{children}</div>
     </div>
--- components/AppLayout/index.js
+++ components/AppLayout/index.js
@@ -1,2 +1,1 @@
-import * from "./AppLayout";
-
+export * from "./AppLayout";
