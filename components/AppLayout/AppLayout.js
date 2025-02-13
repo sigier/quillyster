cc06e12143a4814cf044b0d128169d955186f723
sigier <jotulw@proton.me>
2025-02-13 12:09:25 +0200

    style: updated sidebar buttons appearance

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -1,16 +1,26 @@
 import Link from "next/link";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoins } from "@fortawesome/free-solid-svg-icons";
 
 export const AppLayout = ({ children }) => {
   const { user } = useUser();
   return (
     <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
       <div className="flex flex-col overflow-hidden text-white">
-        <div className="bg-slate-800">
+        <div className="bg-slate-800 px-2">
           <div>lofo</div>
-          <div>cta</div>
-          <div>tokens</div>
+          <Link
+            className="bg-green-500 tracking-wider w-full text-center text-white font-bold cursor-pointer uppercase px-4 py-2 rounded-md hover:bg-green-600 transition-colors block"
+            href="/post/new"
+          >
+            New post
+          </Link>
+          <Link className="block mt-2 text-center" href="/topup">
+            <FontAwesomeIcon icon={faCoins} className="text-yellow-500" />
+            <span className="pl-1">0 tokens</span>
+          </Link>
         </div>
         <div className="flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-600">
           List post
--- pages/topup.js
+++ pages/topup.js
@@ -1,8 +1,13 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { AppLayout } from "../components/AppLayout/AppLayout";
 
 export default function Topup(props) {
   return <div>This Topup</div>;
 }
+
+Topup.getLayout = function getLayout(page, pageProps) {
+  return <AppLayout {...pageProps}>{page}</AppLayout>;
+};
 
 export const getServerSideProps = withPageAuthRequired(() => {
   return {
